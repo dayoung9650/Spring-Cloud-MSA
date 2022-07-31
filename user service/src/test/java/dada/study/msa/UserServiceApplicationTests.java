43b8bb315388bda21e4c8cbaccb6fe1b1dd68796
김다영 <dada@gimdayeong-ui-MacBookPro.local>
2022-07-31 17:15:22 +0900

    initial commit

---
+++ user service/src/test/java/dada/study/msa/UserServiceApplicationTests.java
@@ -0,0 +1,14 @@
+package dada.study.msa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootTest
+class UserServiceApplicationTests {
+	@Test
+	void contextLoads() {
+	}
+
+}
